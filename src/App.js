fec0c985c2485542590f3dabdf65c4ed67d543cf
Ekip Kalir <ekipkalir@gmail.com>
2021-03-16 15:19:46 +0000

    REFACTORED

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
+import Accordion from './Accordion';
+import { accordionData } from './utils/content';
 
 const App = () => {
-  const accordionData = {
-    title: 'Section 1',
-    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-      laborum cupiditate possimus labore, hic temporibus velit dicta earum
-      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-      voluptatem.`
-  };
-
-  const { title, content } = accordionData;
-
   return (
-    <React.Fragment>
-      <h1>React Accordion Demo</h1>
+    <div>
+      <h1>React Accordion Menu Exercise</h1>
       <div className="accordion">
-        <div className="accordion-item">
-          <div className="accordion-title">
-            <div>{title}</div>
-            <div>+</div>
-          </div>
-          <div className="accordion-content">{content}</div>
-        </div>
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
